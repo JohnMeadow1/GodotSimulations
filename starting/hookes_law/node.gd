35c0800ff2c81f50753d550ce64a493652c9fc5c
andrzej <andrzej@Lancre>
2018-03-08 09:38:46 +0100

    New starting problem

--- starting/hookes_law/node.gd
+++ starting/hookes_law/node.gd
@@ -1,46 +1,46 @@
 extends Node2D
 
-var velocity = Vector2(0.0,0.0)
-var old_position = Vector2(0.0,0.0)
-var mass = 1.0
-# reciprocal mass mu = 1./mass
-var mu = 1.0
+var velocity          = Vector2( 0.0, 0.0 )
+var previous_position = Vector2( 0.0, 0.0 )
+var F                 = Vector2( 0.0, 0.0 )
 
-# arrays with neighbours, as well as the corresponding constants
+var mass      = 1.0
+var mu        = 1.0 # 1./mass
 var neighbors = []
-var ks = []
-var ls = []
+var ks        = []
+var ls        = []
 
-# calulation of step_{-1}
+export var is_static = false
+
 func _ready():
-	old_position = position - velocity*get_physics_process_delta_time()
+	previous_position = position - velocity * get_physics_process_delta_time()
+	if is_static:
+		get_node("sprite").modulate = Color( 1.0, 0.0, 0.0 )
 
-# calculation of the force
-func force():
-	var F = Vector2(0.0, 0.0)
-	# TODO: calculation of the force from Hooke's Law
-	return F
-
-# using verlet algorithm
 func _physics_process(delta):
-	verlet(delta)
+	if !is_static:
+		verlet(delta)
 
-# smart setters
+
 func set_velocity(v):
-	velocity = v
-	old_position = position - velocity*get_physics_process_delta_time()
+	velocity          = v
+	previous_position = position - velocity * get_physics_process_delta_time()
 
 func set_mass(m):
 	mass = m
-	mu = pow(m,-1.0)
+	mu   = pow( m, -1.0 )
 
-# integrating algorithms: euler and verlet
 func euler(delta):
-	velocity += force()*mu*delta
-	position += velocity*delta
+	velocity += force() * mu * delta
+	position += velocity * delta
 
 func verlet(delta):
-	var new_position = 2*position - old_position + force()*mu*pow(delta,2.0)
-	old_position = position
-	position = new_position
-	velocity = (position - old_position)/delta
+	var new_position  = 2 * position - previous_position + force() * mu * pow( delta , 2.0 )
+	previous_position = position
+	position          = new_position
+	velocity          = ( position - previous_position ) / delta
+	
+func force():
+	F = Vector2( 0.0, 0.0 )
+	# TODO: Calculation of forces
+	return F 
